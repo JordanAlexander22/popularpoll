f19c3525ea1d636e77f4e25ae68a162befcf19d0
JordanAlexander22 <jordancovo1@gmail.com>
2020-08-10 18:24:01 -0700

    reorganizing mongoose code

--- server/index.js
+++ server/index.js
@@ -9,16 +9,16 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT;
-const uri = process.env.ATLAS_URI;
+const db = require('./models');
+// mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true
+// }
+// )
+// const connection = mongoose.connection;
 
-mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true
-}
-)
-const connection = mongoose.connection;
+// connection.once('open', () => {
+//     console.log("MongoDB database connection established successfully");
+// });
 
-connection.once('open', () => {
-    console.log("MongoDB database connection established successfully");
-});
 
 app.use(cors()); 
 app.use(bodyParser.urlencoded({
--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,21 @@
 const mongoose = require('mongoose');
+const uri = process.env.ATLAS_URI;
+
 
 mongoose.set('debug', true); //log out all database transactions
 
 mongoose.Promise= global.Promise; //enables promises and async
 
-mongoose.connect('')
+
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true
+}
+)
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+});
+
+module.exports.Users = require('./Users');
+
+module.exports.Polls = require('./Polls');
