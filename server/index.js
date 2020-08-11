2f9385513c884a64fc2d214362073167559af4f7
JordanAlexander22 <jordancovo1@gmail.com>
2020-08-10 18:08:22 -0700

    connected to database

--- server/index.js
+++ server/index.js
@@ -1,13 +1,24 @@
-require('dotenv').config()
-
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
 
 const handle= require('./handlers')
 
+require('dotenv').config();
+
 const app = express();
 const port = process.env.PORT;
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true
+}
+)
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+});
 
 app.use(cors()); 
 app.use(bodyParser.urlencoded({
@@ -19,6 +30,7 @@ app.get('/', (req, res) => res.json({hello: 'world'}))
 app.use(handle.notFound)
 
 app.use(handle.errors)
+
 
 app.listen(port, console.log(`server listening on ${port}`))
 
