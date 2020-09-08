929decaf311d3f35fb875c1b49d3e9eceef634c0
JordanAlexander22 <jordancovo1@gmail.com>
2020-09-07 18:38:01 -0700

    setting up axios

---
+++ client/src/services/api.js
@@ -0,0 +1,19 @@
+//logic stored here to be used throughout components
+
+import axios from 'axios';
+
+
+export const setToken = token => {
+    if (token) {
+        axios.defaults.headers.common
+    }
+}
+
+
+
+export const call = async (method, path, data) => {
+    const response = await axios{method}(`/${path}`, data);
+    return response.data;
+}
+
+export default {call};
